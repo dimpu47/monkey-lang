6c466b8d7c19fa158f0e5c2f421b633d9332f016
James Mills <prologic@shortcircuit.net.au>
2018-11-04 22:46:02 +1000

    2.4 Parser (basic)

---
+++ ast/ast.go
@@ -0,0 +1,62 @@
+package ast
+
+// Packge ast implement the Abstract Syntax Tree that represents the parsed
+// source code before being passed on to the interpreter for evaluation.
+
+import (
+	"git.mills.io/prologic/monkey/token"
+)
+
+// Node defines an interface for all nodes in the AST.
+type Node interface {
+	TokenLiteral() string
+}
+
+// Statement defines the interface for all statement nodes.
+type Statement interface {
+	Node
+	statementNode()
+}
+
+// Expression defines the interface for all expression nodes.
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Program is the root node. All programs consist of a slice of Statement(s)
+type Program struct {
+	Statements []Statement
+}
+
+// TokenLiteral prints the literal value of the token associated with this node
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+	return ""
+}
+
+// LetStatement the `let` statement represents the AST node that binds an
+// expression to an identifier
+type LetStatement struct {
+	Token token.Token // the token.LET token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {}
+
+// TokenLiteral prints the literal value of the token associated with this node
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+// Identifier is a node that holds the literal value of an identifier
+type Identifier struct {
+	Token token.Token // the token.IDENT token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {}
+
+// TokenLiteral prints the literal value of the token associated with this node
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
---
+++ parser/parser.go
@@ -0,0 +1,97 @@
+package parser
+
+// Package parser implements the parser that takes as input tokens from the
+// lexer and produces as output an AST (Abstract Syntax Tree)
+
+import (
+	"git.mills.io/prologic/monkey/ast"
+	"git.mills.io/prologic/monkey/lexer"
+	"git.mills.io/prologic/monkey/token"
+)
+
+// Parser implements the parser
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+// New constructs a new Parser with a Lexer as input
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// Read two tokens, so curToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+// ParseProgram starts the parsing process and is the entry point for all other
+// sub-parsers that are responsible for other nodes in the AST.
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: We're skipping the expressions until we
+	// encounter a semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t token.Type) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.Type) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.Type) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken()
+		return true
+	}
+	return false
+}
