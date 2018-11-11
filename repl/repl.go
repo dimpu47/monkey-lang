877476e35cb4d7a47ae3752a2be109c532423533
James Mills <prologic@shortcircuit.net.au>
2018-11-12 00:36:55 +1000

    2.9 REPL (read-parse-print-loop)

--- repl/repl.go
+++ repl/repl.go
@@ -9,11 +9,26 @@ import (
 	"io"
 
 	"git.mills.io/prologic/monkey/lexer"
-	"git.mills.io/prologic/monkey/token"
+	"git.mills.io/prologic/monkey/parser"
 )
 
 // PROMPT is the REPL prompt displayed for each input
 const PROMPT = ">> "
+
+// MonkeyFace is the REPL's face of shock and horror when you encounter a
+// parser error :D
+const MonkeyFace = `            __,__
+   .--.  .-"     "-.  .--.
+  / .. \/  .-. .-.  \/ .. \
+ | |  '|  /   Y   \  |'  | |
+ | \   \  \ 0 | 0 /  /   / |
+  \ '- ,\.-"""""""-./, -' /
+   ''-' /_   ^ ^   _\ '-''
+       |  \._   _./  |
+       \   \ '~' /   /
+        '._ '-=-' _.'
+           '-----'
+`
 
 // Start starts the REPL in a continious loop
 func Start(in io.Reader, out io.Writer) {
@@ -28,9 +43,24 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MonkeyFace)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
