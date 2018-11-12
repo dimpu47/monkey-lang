ea753dee534fc33900db4b5ab7c9d93fc4e48176
James Mills <prologic@shortcircuit.net.au>
2018-11-13 00:13:47 +1000

    4.6 Hello World

--- eval/builtins.go
+++ eval/builtins.go
@@ -1,6 +1,8 @@
 package eval
 
 import (
+	"fmt"
+
 	"git.mills.io/prologic/monkey/object"
 )
 
@@ -103,6 +105,15 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
