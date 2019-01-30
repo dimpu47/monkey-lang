4275fea6de8c65df441e0b055f8665500970afc5
James Mills <1290234+prologic@users.noreply.github.com>
2019-01-30 23:18:23 +1000

    Fixed recursive function lookups

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -186,12 +186,13 @@ func (c *Compiler) Compile(node ast.Node) error {
 		}
 
 	case *ast.LetStatement:
+		symbol := c.symbolTable.Define(node.Name.Value)
+
 		err := c.Compile(node.Value)
 		if err != nil {
 			return err
 		}
 
-		symbol := c.symbolTable.Define(node.Name.Value)
 		if symbol.Scope == GlobalScope {
 			c.emit(code.BindGlobal, symbol.Index)
 		} else {
